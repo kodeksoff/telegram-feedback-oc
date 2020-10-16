d83b44f4c9ca5f65004529e5a329a318f3ff41d7
kodeksoff <kodeksoff@gmail.com>
2020-10-16 12:35:58 +0300

    11111

--- Plugin.php
+++ Plugin.php
@@ -8,7 +8,7 @@ class Plugin extends PluginBase
     public function registerComponents()
     {
         return [
-            \undirezt\telegramfeedback\components\TelegramFeedback::class => 'telegramFeedbackjhgjk'
+            \undirezt\telegramfeedback\components\TelegramFeedback::class => 'telegramFeedbacktestco'
         ];
     }
 
